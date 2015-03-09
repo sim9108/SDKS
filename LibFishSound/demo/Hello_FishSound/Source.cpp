205f1ac148a931dc9ed8a21a87522cb3b90716df
sim9108 <sim9108@naver.com>
2015-03-09 16:02:16 +0900

    fishSound 샘플 추가

--- LibFishSound/demo/Hello_FishSound/Source.cpp
+++ LibFishSound/demo/Hello_FishSound/Source.cpp
@@ -2,34 +2,62 @@
 #include <ogg\ogg.h>
 #include<fishsound\fishsound.h>
 #include <iostream>
+#include <sndfile.h>
 
 //Ogg Flac, speex-->wav
 //test.exe test.ogg test.wav
 
-long decode_serailno = -1;
+static char * infilename, *outfilename;
+static int begun = 0;
+static FishSoundInfo fsinfo;
+static SNDFILE * sndfile;
 
-int decoded_float(FishSound* fsound, float** pcm, long frames, void* user_data)
+long decode_serialno = -1;
+
+static int
+open_output(int samplerate, int channels)
 {
+	SF_INFO sfinfo;
+
+	sfinfo.samplerate = samplerate;
+	sfinfo.channels = channels;
+	sfinfo.format = SF_FORMAT_WAV | SF_FORMAT_PCM_16;
+	sndfile = sf_open(outfilename, SFM_WRITE, &sfinfo);
+
 	return 0;
 }
 
-int read_packet(OGGZ * oggz, oggz_packet * op, long serialno,
+int decoded_float(FishSound* fsound, float** pcm, long frames, void* user_data)
+{
+	if (!begun) {
+		fish_sound_command(fsound, FISH_SOUND_GET_INFO, &fsinfo,
+			sizeof (FishSoundInfo));
+		open_output(fsinfo.samplerate, fsinfo.channels);
+		begun = 1;
+	}
+
+	sf_writef_float(sndfile, (float *)pcm, frames);
+
+	return 0;
+}
+
+int read_packet(OGGZ * oggz, oggz_packet * opm, long serialno,
 	void * user_data){
 
-	FishSound* fsound = static_cast<FishSound*>(user_data);
+	FishSound * fsound = (FishSound *)user_data;
+	ogg_packet* op = &opm->op;
 
-	if (decode_serailno == -1 && op -> op.b_o_s && op -> op.bytes >= 8){
-		if (fish_sound_identify(op->op.packet, op->op.bytes) != FISH_SOUND_UNKNOWN){
-			decode_serailno = serialno;
-		}
+	if (decode_serialno == -1 && op->b_o_s && op->bytes >= 8) {
+		if (fish_sound_identify(op->packet, op->bytes) != FISH_SOUND_UNKNOWN)
+			decode_serialno = serialno;
 	}
 
-	if (serialno == decode_serailno){
-		fish_sound_prepare_truncation(fsound, static_cast<long>(op->op.granulepos), op->op.e_o_s);
-		fish_sound_decode(fsound, op->op.packet, op->op.bytes);
+	/* If this is the track we are decoding, go ahead and decode it */
+	if (serialno == decode_serialno) {
+		fish_sound_prepare_truncation(fsound, op->granulepos, op->e_o_s);
+		fish_sound_decode(fsound, op->packet, op->bytes);
 	}
 
-	std::cout << "ok good" << std::endl;
 	return 0;
 }
 
@@ -40,8 +68,8 @@ int main(int argc, char** argv)
 		return 0;
 	}
 
-	char* infilename = argv[1];
-	char* outfilename = argv[2];
+	infilename = argv[1];
+	outfilename = argv[2];
 
 	FishSound* fsound = fish_sound_new(FISH_SOUND_DECODE, nullptr);//!!  1
 	fish_sound_set_interleave(fsound, 1);
@@ -59,5 +87,7 @@ int main(int argc, char** argv)
 	//////////////////////////////////////
 
 	fish_sound_delete(fsound);
+
+	sf_close(sndfile);
 	return 0;
 }
