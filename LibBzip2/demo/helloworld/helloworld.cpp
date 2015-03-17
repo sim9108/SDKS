a50c41ad7b0af5f4d995b67922183555d192e173
sim9108 <sim9108@naver.com>
2015-03-17 22:50:44 +0900

    no message

--- LibBzip2/demo/helloworld/helloworld.cpp
+++ LibBzip2/demo/helloworld/helloworld.cpp
@@ -9,31 +9,34 @@
 
 int _tmain(int argc, _TCHAR* argv[]){
 	const int BUF = 1024;	
+	int bzerror;
+	BZFILE* bz = nullptr;
 
-	FILE* fp= fopen("myfile.txt.bz2", "w");	if (!fp){		std::cout << "file io error" << std::endl;		return 0;	}	int bzerror;
-	int blockSize100k{ 9 };
-	int verbosity{ 0 };
-	int workFactor{ 30 };
+	try{
+		FILE* fp = fopen("myfile.txt.bz2", "w");		if (!fp){			std::cout << "file io error" << std::endl;			return 0;		}		
+		int blockSize100k{ 9 };
+		int verbosity{ 0 };
+		int workFactor{ 30 };
 
-	BZFILE* bz = BZ2_bzWriteOpen(&bzerror, fp, blockSize100k, verbosity, workFactor);
+		bz = BZ2_bzWriteOpen(&bzerror, fp, blockSize100k, verbosity, workFactor);
+		if (BZ_OK != bzerror) throw "error";
 
-	unsigned int  nbytes_in_lo32, nbytes_in_hi32;
-	unsigned int  nbytes_out_lo32, nbytes_out_hi32;
-	if (bzerror != BZ_OK) {
-		BZ2_bzWriteClose64(&bzerror, bz, 1,	&nbytes_in_lo32, &nbytes_in_hi32,&nbytes_out_lo32, &nbytes_out_hi32);
-		std::cout << "error" << std::endl;
+		std::string datas = "hello world! æ»≥Á«œººø‰.";
+		do {
+			BZ2_bzWrite(&bzerror, bz, reinterpret_cast<void*>(const_cast<char*>((datas.data()))), datas.size());
+			if (BZ_IO_ERROR == bzerror) throw "error";
+		} while (false);		unsigned int  nbytes_in_lo32, nbytes_in_hi32;
+		unsigned int  nbytes_out_lo32, nbytes_out_hi32;		BZ2_bzWriteClose64(&bzerror, bz, 0, &nbytes_in_lo32, &nbytes_in_hi32, &nbytes_out_lo32, &nbytes_out_hi32);
 		return 0;
-	};
-	
-	std::string datas = "hello world! æ»≥Á«œººø‰.";
-	do {
-		BZ2_bzWrite(&bzerror, bz, reinterpret_cast<void*>( const_cast<char*>((datas.data()) ), datas.size());
-		if (bzerror == BZ_IO_ERROR) {
-			BZ2_bzWriteClose64(&bzerror, bz, 1,	&nbytes_in_lo32, &nbytes_in_hi32,&nbytes_out_lo32, &nbytes_out_hi32);
-			std::cout << "error" << std::endl;
-			return 0;
-		}
-	} while (false);		BZ2_bzWriteClose64(&bzerror, bz, 0,	&nbytes_in_lo32, &nbytes_in_hi32,&nbytes_out_lo32, &nbytes_out_hi32);
-	return 0;
+
+	}
+	catch (...){
+		unsigned int  nbytes_in_lo32, nbytes_in_hi32;
+		unsigned int  nbytes_out_lo32, nbytes_out_hi32;
+		BZ2_bzWriteClose64(&bzerror, bz, 1, &nbytes_in_lo32, &nbytes_in_hi32, &nbytes_out_lo32, &nbytes_out_hi32);
+		std::cout << "error" << std::endl;
+
+		return 0;
+	}
 }
 
