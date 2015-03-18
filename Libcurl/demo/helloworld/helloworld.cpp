018c4c3eed264dac51a59d6eeee236786fa353a2
sim9108 <sim9108@naver.com>
2015-03-18 13:11:26 +0900

    lib curl 라이브러리 포팅 버그 수정 및 샘플 추가

---
+++ Libcurl/demo/helloworld/helloworld.cpp
@@ -0,0 +1,57 @@
+// helloworld.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <curl/curl.h>
+#include <string>
+
+static size_t write_data(void *ptr, size_t size, size_t nmemb, void *stream)
+{
+	std::string* mdata = (std::string*)stream;
+
+	size_t written = size*nmemb;
+	mdata->reserve(mdata->size() + written);
+	
+	char* temp = reinterpret_cast<char*>(ptr);
+	for (int i = 0; i<written; ++i){
+		mdata->push_back(temp[i]);
+	}
+
+	return written;
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	curl_global_init(CURL_GLOBAL_ALL);
+	std::string datas;
+
+	
+
+	CURL* curl = curl_easy_init();
+	if (!curl){
+		std::cout << "error" << std::endl;
+		return 0;
+	}
+
+	curl_easy_setopt(curl, CURLOPT_URL, "http://google.com");
+	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+	curl_easy_setopt(curl, CURLOPT_WRITEDATA, &datas);
+
+	CURLcode rc = curl_easy_perform(curl);
+	if (CURLE_OK != rc){
+		std::cout << "result:" << curl_easy_strerror(rc) << std::endl;
+	}
+	else{
+		std::cout << datas << std::endl;
+	}
+	
+
+	curl_easy_cleanup(curl);
+
+
+
+
+	return 0;
+}
+
