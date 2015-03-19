112f1532cea016847ded2cb1f5d53633f14003ad
sim9108 <sim9108@naver.com>
2015-03-19 22:07:20 +0900

    SFML 샘플 추가

---
+++ LibSFML/demo/helloworld/helloworld.cpp
@@ -0,0 +1,38 @@
+// helloworld.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	sf::RenderWindow window;
+	window.create(sf::VideoMode(800, 600), "My First Sfmal Game");
+	
+	sf::Texture pTexture;
+	sf::Sprite playerImage;
+
+	if (!pTexture.loadFromFile("player.png")){
+		std::cout << "Error image file" << std::endl;
+	}
+
+	playerImage.setTexture(pTexture);
+
+	while (window.isOpen())
+	{
+		sf::Event Event;
+		while (window.pollEvent(Event))
+		{
+			if (sf::Event::Closed == Event.type){
+				window.close();
+			}
+		}
+		window.draw(playerImage);
+		window.display();
+	}
+
+	return 0;
+}
+
