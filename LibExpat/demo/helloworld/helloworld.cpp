6408cef78e7250e63bfeebfa4fcccaa5c75de94b
sim9108 <sim9108@naver.com>
2015-03-19 12:09:51 +0900

    no message

--- LibExpat/demo/helloworld/helloworld.cpp
+++ LibExpat/demo/helloworld/helloworld.cpp
@@ -3,32 +3,40 @@
 
 #include "stdafx.h"
 #include <iostream>
-
 #include <expat.h>
 #include <fstream>
 #include <sstream>
+#include <cctype>
+#include <string>
+#include <deque>
+#include <map>
+#include <vector>
+
+struct UNITDATA{
+	std::string attr_name_;
+	std::string data_;	
+};
 
 struct USER_DATA{
-	int depth_;
+	std::deque<std::string> node_;
+	std::map<std::string, std::vector<UNITDATA>> values_;
 };
 
 void XMLCALL start(void *user_data, const char *name, const char **attr);
 void XMLCALL end(void *user_data, const char *name);
-
+void XMLCALL char_data(void *user_data, const XML_Char *data, int len);
 
 int _tmain(int argc, _TCHAR* argv[]){
-
 	XML_Parser ex = XML_ParserCreate(nullptr);
 	if (!ex){
 		std::cout << "xml parser error" << std::endl;
 		return 0;
 	}
 
-	USER_DATA data;
-	data.depth_ = 0;
-
-	XML_SetUserData(ex, &data);
+	USER_DATA result;
+	XML_SetUserData(ex, &result);
 	XML_SetElementHandler(ex, start, end);
+	XML_SetCharacterDataHandler(ex, char_data);
 
 	std::ifstream rp("test.xml", std::ios_base::binary);
 	const int BUF = 1024;
@@ -48,29 +56,61 @@ int _tmain(int argc, _TCHAR* argv[]){
 
 	XML_ParserFree(ex);
 
+	for (auto& item : result.values_){
+		std::cout << item.first<<std::endl;
+		for (auto& vitem : item.second){
+			std::cout <<"\t"<< vitem.attr_name_ << "\t:" << vitem.data_ << std::endl;
+		}
+
+		std::cout << std::endl;
+	}
+
 	return 0;
 }
 
 void XMLCALL
 start(void *user_data, const char *name, const char **attr){
 	USER_DATA* udata = static_cast<USER_DATA*>(user_data);
-	std::stringstream ss;
 	
-	for (int i = 0; i < udata->depth_; i++)
-		ss << "\t";
+	udata->node_.push_back(name);
+	std::stringstream node_name;
+	for (auto& item : udata->node_){
+		node_name << item << "/";
+	}	
 
-	ss << name<<" ";
 	for (int i = 0; attr[i]; i += 2) {
-		ss << attr[i] << "=" << attr[i + 1]<<" ";
+		UNITDATA unit_data;
+		unit_data.attr_name_ = attr[i];
+		unit_data.data_ = attr[i + 1];
+		udata->values_[node_name.str()].push_back(unit_data);
 	}
-	ss << std::endl;
-
-	std::cout << ss.str();
-	udata->depth_++;
 }
 
 void XMLCALL
 end(void *user_data, const char *name){
 	USER_DATA* udata = static_cast<USER_DATA*>(user_data);
-	udata->depth_--;
+	udata->node_.pop_back();
 }
+
+void XMLCALL 
+char_data(void *user_data, const XML_Char *data, int len){
+	if (1 == len && std::isspace(*data) ) return;
+	USER_DATA* udata = static_cast<USER_DATA*>(user_data);
+
+	std::stringstream node_name;
+	for (auto& item : udata->node_){
+		node_name << item << "/";
+	}
+
+	UNITDATA unit_data;
+	unit_data.data_ = std::string(data, len);
+	udata->values_[node_name.str()].push_back(unit_data);
+}
+
+
+
+
+
+
+
+
