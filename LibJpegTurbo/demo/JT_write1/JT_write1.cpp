6389b06d0d70a210c9ba311eceb41a61ade5e9aa
sim9108 <sim9108@naver.com>
2015-05-15 22:12:26 +0900

    no message

---
+++ LibJpegTurbo/demo/JT_write1/JT_write1.cpp
@@ -0,0 +1,45 @@
+// JT_write1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <turbojpeg.h>
+#include <vector>
+#include <fstream>
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	///// feak start 
+	auto width = 320;
+	auto height = 240;
+	auto compoents = 3;//rgb
+	auto row_stirde = width*compoents;
+
+	std::vector<unsigned char> datas(row_stirde * height);
+
+	int val{ 0 };
+	for (auto& item : datas){
+		item = (++val) % 256;
+	}
+	//////// feak end...
+
+	unsigned char * output = tjAlloc(TJBUFSIZE(width, height));
+	auto handleCompress = tjInitCompress();
+	unsigned long size = 0;
+	int  flags = 0, jpegsubsamp = 0;
+	int jpegQuality = 100;
+	auto ret = tjCompress(handleCompress, datas.data(), width, row_stirde, height, compoents, output, &size, jpegsubsamp, jpegQuality, flags);
+	tjDestroy(handleCompress);
+
+	do{
+		if (-1 == ret) break;
+		std::ofstream fp("test.jpg",std::ios_base::binary);
+		if (!fp) break;
+		fp.write( reinterpret_cast<char*>(output), size);
+		fp.close();
+	} while (false);
+
+
+	tjFree(output);
+	return 0;
+}
+
