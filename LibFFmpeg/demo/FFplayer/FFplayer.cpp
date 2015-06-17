4718893abc2375d77182ce94f87c204a40180ab4
sim9108 <sim9108@naver.com>
2015-06-17 17:09:10 +0900

    no message

---
+++ LibFFmpeg/demo/FFplayer/FFplayer.cpp
@@ -0,0 +1,165 @@
+﻿#include "stdafx.h"
+
+extern "C"{
+	#include <libavcodec/avcodec.h>
+	#include <libavformat/avformat.h>
+	#include <libswscale/swscale.h>
+}
+
+#include <SDL.h>
+#include <SDL_thread.h>
+
+#undef main 
+
+int main(int argc, char** argv){
+	if (argc < 2) {
+		fprintf(stderr, "Usage: test <file>\n");
+		exit(1);
+	}
+
+	// Register all formats and codecs
+	av_register_all();
+
+	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER)) {
+		fprintf(stderr, "Could not initialize SDL - %s\n", SDL_GetError());
+		exit(1);
+	}
+
+	AVFormatContext *pFormatCtx = nullptr;
+	if (avformat_open_input(&pFormatCtx, argv[1], NULL, NULL) != 0)
+		return -1; 
+
+	// Retrieve stream information
+	if (avformat_find_stream_info(pFormatCtx, NULL)<0)
+		return -1; 
+
+	// Dump information about file onto standard error
+	av_dump_format(pFormatCtx, 0, argv[1], 0);
+
+	// Find the first video stream
+	unsigned int videoStream = -1;
+	for (unsigned int i = 0; i<pFormatCtx->nb_streams; i++)
+	if (pFormatCtx->streams[i]->codec->codec_type == AVMEDIA_TYPE_VIDEO) {
+		videoStream = i;
+		break;
+	}
+	if (videoStream == -1)
+		return -1; // Didn't find a video stream
+
+	// Get a pointer to the codec context for the video stream
+	AVCodecContext *pCodecCtx = pFormatCtx->streams[videoStream]->codec;
+
+	// Find the decoder for the video stream
+	AVCodec *pCodec = avcodec_find_decoder(pCodecCtx->codec_id);
+	if (pCodec == NULL) {
+		fprintf(stderr, "Unsupported codec!\n");
+		return -1; // Codec not found
+	}
+
+	// Open codec
+	AVDictionary* optionsDict = nullptr;
+	if (avcodec_open2(pCodecCtx, pCodec, &optionsDict)<0)
+		return -1; // Could not open codec
+
+	// Allocate video frame
+	AVFrame *pFrame = avcodec_alloc_frame();
+
+	AVFrame *pFrameYUV = avcodec_alloc_frame();
+	if (pFrameYUV == NULL)
+		return -1;
+	
+	SDL_Window* screen = SDL_CreateWindow("My Game Window",
+											SDL_WINDOWPOS_UNDEFINED,
+											SDL_WINDOWPOS_UNDEFINED,
+											pCodecCtx->width, pCodecCtx->height,
+											SDL_WINDOW_OPENGL);
+
+	SDL_Renderer *renderer = SDL_CreateRenderer(screen, -1, 0);
+	
+	if (!screen) {
+		fprintf(stderr, "SDL: could not set video mode - exiting\n");
+		exit(1);
+	}
+	
+	SDL_Texture    *bmp = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_IYUV, SDL_TEXTUREACCESS_STREAMING, pCodecCtx->width, pCodecCtx->height);
+
+	SwsContext* sws_ctx =
+		sws_getContext
+		(
+		pCodecCtx->width,
+		pCodecCtx->height,
+		pCodecCtx->pix_fmt,
+		pCodecCtx->width,
+		pCodecCtx->height,
+		PIX_FMT_YUV420P,
+		SWS_BILINEAR,
+		NULL,
+		NULL,
+		NULL
+		);
+
+	int numBytes = avpicture_get_size(PIX_FMT_YUV420P, pCodecCtx->width,
+		pCodecCtx->height);
+	uint8_t* buffer = (uint8_t *)av_malloc(numBytes*sizeof(uint8_t));
+
+	avpicture_fill((AVPicture *)pFrameYUV, buffer, PIX_FMT_YUV420P,
+		pCodecCtx->width, pCodecCtx->height);
+	
+	SDL_Rect rect = { 0, 0, pCodecCtx->width, pCodecCtx->height };
+	AVPacket  packet;
+	int frameFinished{};
+	SDL_Event event;
+
+	while (av_read_frame(pFormatCtx, &packet) >= 0) {
+		// Is this a packet from the video stream?
+		if (packet.stream_index == videoStream) {
+			// Decode video frame
+			avcodec_decode_video2(pCodecCtx, pFrame, &frameFinished,&packet);
+			if (!frameFinished) {
+				SDL_Delay(50);
+				continue;
+			}
+
+			sws_scale
+				(
+				sws_ctx,
+				(uint8_t const * const *)pFrame->data,
+				pFrame->linesize,
+				0,
+				pCodecCtx->height,
+				pFrameYUV->data,
+				pFrameYUV->linesize
+				);
+			////iPitch
+			SDL_UpdateTexture(bmp, &rect, pFrameYUV->data[0], pFrameYUV->linesize[0]);
+			SDL_RenderClear(renderer);
+			SDL_RenderCopy(renderer, bmp, &rect, &rect);
+			SDL_RenderPresent(renderer);
+			SDL_Delay(20);
+		}
+
+		// Free the packet that was allocated by av_read_frame
+		av_free_packet(&packet);
+	
+		SDL_PollEvent(&event);
+		switch (event.type) {
+		case SDL_QUIT:
+			SDL_Quit();
+			exit(0);
+			break;
+		default:
+			break;
+		}
+	}
+
+	SDL_DestroyTexture(bmp);
+
+	av_free(pFrame);
+	av_free(pFrameYUV);
+
+	avcodec_close(pCodecCtx);
+
+	avformat_close_input(&pFormatCtx);
+	return 0;
+}
+
